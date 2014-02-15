c816f83856d1b506686bd0147941a84c69e3b2f4
LeonidasKyrkos <leo.kyrkos@gmail.com>
2014-02-15 19:22:43 +0000

    moviedbfirstcommit

---
+++ mymoviedb/Controllers/helloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mymoviedb.Controllers
+{
+    public class helloWorldController : Controller
+    {
+        //
+        // GET: /helloWorld/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /helloWorld/Welcome/
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.numTimes = numTimes;
+
+            return View();
+        }
+    }
+}
