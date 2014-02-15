21a05a0c2b2da6a99348aa696973f5d2b5e2d880
LeonidasKyrkos <leo.kyrkos@gmail.com>
2014-02-15 19:50:27 +0000

    moviedbedit3

--- mymoviedb/Controllers/HomeController.cs
+++ mymoviedb/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace mymoviedb.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to Leo's Movie Database!";
 
             return View();
         }
